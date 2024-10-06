482ceb21237de4654945b4f1b2d5bce7d83a82a9
rajvir <rajvirvirdi2000@gmail.com>
2024-10-06 19:38:26 +0530

    changes in UI

--- Client/src/components/utilities/Suggestion.jsx
+++ Client/src/components/utilities/Suggestion.jsx
@@ -8,7 +8,7 @@ const Suggestion = ({name, url}) => {
             <img src={url} alt="userImage" className='block h-9 w-9 rounded-full object-cover'/>
             <p>{name}</p>  
         </div>
-        <button className='py-2 pl-2 pr-3.5 text-sm font-semibold rounded bg-blue-600 hover:bg-blue-700 text-white'><AddRoundedIcon sx={{mb : 0.4, fontSize : 15}}/> Add Friend</button>  
+        <button className='py-2 pl-2 pr-3.5 text-sm font-semibold rounded bg-blue-600 hover:bg-blue-700 text-white'><AddRoundedIcon sx={{mb : 0.4, fontSize : 15}}/> Follow</button>  
     </div>
   )
 }
